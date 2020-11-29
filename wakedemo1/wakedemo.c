9c88fd7b77c201d2e917280f73f67a3f27d437dc
student <rubenabustamante@gmail.com>
2020-11-29 16:56:04 -0700

    change placement of diamaond state

--- wakedemo1/wakedemo.c
+++ wakedemo1/wakedemo.c
@@ -14,11 +14,53 @@ void wdt_c_handler()
 {
   static int secCount = 0;
   secCount ++;
-          
-   if (secCount == 250 && switch_state_changed <=4) {/* once/sec */
-     secCount = 0;
-     drawString11x16(20,20, "hello", fontFgColor, COLOR_BLUE);
-     static char state=0;
+  if( secCount ==  250){
+    secCount =0;
+    fontFgColor = (fontFgColor == COLOR_GREEN) ? COLOR_BLACK : COLOR_GREEN;
+    redrawScreen = 1;
+  }
+
+  if(secCount == 250 && switch_state_changed ==0){
+    switch_state();
+     redrawScreen = 0;
+     secCount ==0;
+     
+}
+if(secCount == 250  && switch_state_changed ==1){
+    switch_state();
+    secCount =0;
+     redrawScreen = 0;
+    
+}
+
+if(secCount == 125 && switch_state_changed ==2){
+    switch_state();
+    secCount =0;
+     redrawScreen = 0;
+    
+}
+
+}
+ 
+
+void main()
+{
+  P1DIR |= LED_GREEN;		/**< Green led on when CPU on */		
+  P1OUT |= LED_GREEN;
+  configureClocks();
+  lcd_init();
+  led_init();
+  buzzer_init();
+  switch_init();
+  enableWDTInterrupts();      /**< enable periodic interrupt */
+  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
+  clearScreen(COLOR_BLUE);
+  static char state =0;
+   while (1) {			/* forever */
+   if (redrawScreen) {
+      drawString11x16(20,20, "hello", fontFgColor, COLOR_BLUE);
+     
       switch(state){
       case 0:
 	diamondState();
@@ -39,65 +81,15 @@ void wdt_c_handler()
 	state =0;
 	break;
       }
-      
-   
-   
-   P1OUT |= LED_GREEN;/* green on */
-  
-   }
-   // fontFgColor = (fontFgColor == COLOR_GREEN) ? COLOR_BLACK : COLOR_GREEN;
-   redrawScreen = 1;
-
-  if(secCount == 250 && switch_state_changed ==0){
-    switch_state();
-     redrawScreen = 0;
-     secCount ==0;
-      P1OUT &= ~LED_GREEN;
-}
-if(secCount == 250  && switch_state_changed ==1){
-    switch_state();
-    secCount =0;
-     redrawScreen = 0;
-     P1OUT &= ~LED_GREEN;
-}
-
-if(secCount == 125 && switch_state_changed ==2){
-    switch_state();
-    secCount =0;
-     redrawScreen = 0;
-     P1OUT &= ~LED_GREEN;
-}
-
-}
- 
-
-void main()
-{
-  P1DIR |= LED_GREEN;		/**< Green led on when CPU on */		
-  P1OUT |= LED_GREEN;
-  configureClocks();
-  lcd_init();
-  led_init();
-  buzzer_init();
-  switch_init();
-  enableWDTInterrupts();      /**< enable periodic interrupt */
-  or_sr(0x8);	              /**< GIE (enable interrupts) */
-  
-  clearScreen(COLOR_BLUE);
-  //static char state =0;
-   while (1) {			/* forever */
-   if (redrawScreen) {
-    
-     }
    
       redrawScreen = 0;
-     
-    }
+   }
+   
   
    P1OUT &= ~LED_GREEN;/* green off */
    or_sr(0x10);/**< CPU OFF */
    P1OUT |= LED_GREEN;/* green on */
-   
+   }
 }
 
 
