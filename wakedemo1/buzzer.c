ebfd1569de6c26c8e2c754bc879ae358e4b957b9
student <rubenabustamante@gmail.com>
2020-11-29 20:32:16 -0700

    making sure all files are updated

--- wakedemo1/buzzer.c
+++ wakedemo1/buzzer.c
@@ -1,10 +1,6 @@
 #include <msp430.h>
-
 #include "libTimer.h"
-
 #include "buzzer.h"
-
-
 
 void buzzer_init()
 
--- wakedemo1/led.c
+++ wakedemo1/led.c
@@ -1,21 +1,10 @@
-
-
 #include <msp430.h>
-
 #include "led.h"
 
-
-
 unsigned char red_on = 0, green_on = 0;
-
 unsigned char led_changed = 0;
 
-
-
 static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
-
-
-
 
 
 void led_init()
@@ -23,9 +12,7 @@ void led_init()
 {
 
   P1DIR |= LEDS;// bits attached to leds are output
-
   led_changed = 1;
-
   led_update();
 
 }
@@ -35,12 +22,8 @@ void led_init()
 void led_update()
 
 {
-
   if (led_changed) {
-
     char ledFlags = redVal[red_on] | greenVal[green_on];
-
-
 
     P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
 
